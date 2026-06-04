27daef33236d3a40ac92f894402ee6a1714f3722
Omar Megzari <omarmegzari@users.noreply.github.com>
2026-06-04 12:39:58 +0100

    fix: correction import firebase_auth

--- lib/application/providers/auth_provider.dart
+++ lib/application/providers/auth_provider.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/package_firebase_auth.dart' hide AuthProvider;
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
